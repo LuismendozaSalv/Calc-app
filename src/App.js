3b9fd4bc310ea314537cee70e9f95de893c426df
Luis Mendoza <luismendozasalv@gmail.com>
2022-04-23 08:47:37 -0400

    Firebase

--- src/App.js
+++ src/App.js
@@ -1,37 +1,47 @@
-import Reac, {useState} from 'react';
-import Button from './components/Button';
+/* eslint no-eval: 0 */
+import React, {useState} from 'react';
+import words from 'lodash.words';
 import Functions from './components/Functions';
 import MathOperations from './components/MathOperations';
 import Numbers from './components/Numbers';
 import Result from './components/Result';
 import './App.css';
 const App = () => {
-  const arrayTextoFuncionModificaTexto = useState("")
-  const clickHandlerFunction = text => {
-    console.log("Button.clickHandler", text)
-  }
-  console.log("renderizando de App")
+  //array destructuring
+  const[stack, setStack] = useState("")
+
+  const items = words(stack, /[^-^+^*^/]+/g)
+
+  const value = items.length > 0 ? items[items.length - 1] : 0    ; 
   return (
     <main className="react-calculator">
-      <Result />
-      <Numbers onClickNumber={number =>
-        console.log('Click en number', number) 
-      }/>
+      <Result value={value}/>
+      <Numbers onClickNumber={number => {
+          setStack(`${stack}${number}`)
+        }
+      }/>         
       <Functions 
         onContentClear={() => 
-          console.log('Content clear')
+          setStack("")
         }
-        onDelete = {() =>
+        onDelete = {() =>  {
           console.log('Delete')
-        }
+          if (stack.length > 0) {
+            const newStack = stack.substring(0, stack.length - 1)
+            setStack(newStack)  
+          }
+        }}
         />
       <MathOperations 
-        onClickOperation={operation => 
+        onClickOperation={operation => {
           console.log("Operation:", operation)
-        }
-        onClickEqual={equal =>
+          setStack(`${stack}${operation}`)
+        }}
+        onClickEqual={equal => {
           console.log("Operation:", equal)
-        }/>
+          setStack(eval(stack).toString())
+        }}
+        />
     </main>
   );
 }
